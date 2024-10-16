a6f6b216c56800070208c1d2c9ddc225123cf749
Martin Kihungi <mwangim4786@gmail.com>
2024-10-16 14:39:20 +0300

    utils/redis.js 2

--- utils/redis.js
+++ utils/redis.js
@@ -9,7 +9,6 @@ class RedisClient {
         });
     }
 
-    // check if connection to Redis is a success
     isAlive() {
     if (this.client.connected) {
         return true;
@@ -17,21 +16,18 @@ class RedisClient {
     return false;
     }
 
-    // get value for given key from redis server
     async get(key) {
       const redisGet = promisify(this.client.get).bind(this.client);
       const value = await redisGet(key);
       return value;
     }
   
-    // set key value pair to redis server
     async set(key, value, time) {
       const redisSet = promisify(this.client.set).bind(this.client);
       await redisSet(key, value);
       await this.client.expire(key, time);
     }
   
-    // del key vale pair from redis server
     async del(key) {
       const redisDel = promisify(this.client.del).bind(this.client);
       await redisDel(key);
